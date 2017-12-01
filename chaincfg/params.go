e0e8daad7c9b939e9601a675018b276b7f09074b
limingming <limingming@wident.cn>
2017-12-01 17:15:01 +0800

    change conf

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -607,7 +607,8 @@ var MainNetParams = Params{
 	TicketsPerBlock:         5,
 	TicketMaturity:          128/*256*/,
 	TicketExpiry:            40960, // 5*TicketPoolSize
-	CoinbaseMaturity:        128/*256*/,
+	// CoinbaseMaturity:        128/*256*/,
+	CoinbaseMaturity:        1,
 	SStxChangeMaturity:      1,
 	TicketPoolSizeWeight:    4,
 	StakeDiffAlpha:          1, // Minimal
