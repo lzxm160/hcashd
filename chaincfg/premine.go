ab6dee655283a0f25608ed88d9f86a2442592fbd
limingming <limingming@wident.cn>
2017-12-05 17:33:19 +0800

    change mainnet with simnet

--- chaincfg/premine.go
+++ chaincfg/premine.go
@@ -8,7 +8,7 @@ package chaincfg
 // BlockOneLedgerMainNet is the block one output ledger for the main
 // network.
 var BlockOneLedgerMainNet = []*TokenPayout{
-	// {"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //wancc
+	{"HsMNycPD277U4Zw2qNHNqY8r3MSsnhhiiGW", 10000 * 1e8}, //wancc
 	{"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //fanlq
 	{"HsF4tYLz9JpUFk9aPLC7U2AN8Deq6LkyoWc", 10000 * 1e8}, //shanyl
 	{"HsZKbCUvcpjfHAJpDfWikD7E2oXUGR4ge6q", 10000 * 1e8}, //panc
@@ -21,39 +21,15 @@ var BlockOneLedgerMainNet = []*TokenPayout{
 // BlockOneLedgerTestNet is the block one output ledger for the test
 // network.
 var BlockOneLedgerTestNet = []*TokenPayout{
-	// {"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //wancc
-	{"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //fanlq
-	{"HsF4tYLz9JpUFk9aPLC7U2AN8Deq6LkyoWc", 10000 * 1e8}, //shanyl
-	{"HsZKbCUvcpjfHAJpDfWikD7E2oXUGR4ge6q", 10000 * 1e8}, //panc
-	{"HsNu7JN9SeNb3cH7BJWMiSSpPqY6rz8BSXW", 10000 * 1e8}, //guxy
-	{"HsamDEnZXPRczM4tNTrKbvUZA8fUSe2TqPk", 10000 * 1e8}, //dengcg
-	{"HsLwT4E2ZdqMDwtrtaQKqp98wVaHrJfyEYM", 10000 * 1e8}, //lixm
-	{"HsS6Hqt7yB5Fr2HDBz5gRhK75q7ciuxa7au", 10000 * 1e8}, //yaoyq
 }
 
 // BlockOneLedgerTestNet2 is the block one output ledger for the 2nd test
 // network.
 var BlockOneLedgerTestNet2 = []*TokenPayout{
-	// {"HsTDkyHopJVMqeUeD7mSQc5ViuSvV6aFpzy", 10000 * 1e8}, //wancc
-	{"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //fanlq
-	{"HsF4tYLz9JpUFk9aPLC7U2AN8Deq6LkyoWc", 10000 * 1e8}, //shanyl
-	{"HsZKbCUvcpjfHAJpDfWikD7E2oXUGR4ge6q", 10000 * 1e8}, //panc
-	{"HsNu7JN9SeNb3cH7BJWMiSSpPqY6rz8BSXW", 10000 * 1e8}, //guxy
-	{"HsamDEnZXPRczM4tNTrKbvUZA8fUSe2TqPk", 10000 * 1e8}, //dengcg
-	{"HsLwT4E2ZdqMDwtrtaQKqp98wVaHrJfyEYM", 10000 * 1e8}, //lixm
-	{"HsS6Hqt7yB5Fr2HDBz5gRhK75q7ciuxa7au", 10000 * 1e8}, //yaoyq
 }
 
 // BlockOneLedgerSimNet is the block one output ledger for the simulation
 // network. See under "Hypercash organization related parameters" in params.go
 // for information on how to spend these outputs.
 var BlockOneLedgerSimNet = []*TokenPayout{
-	// {"HsTDkyHopJVMqeUeD7mSQc5ViuSvV6aFpzy", 10000 * 1e8}, //wancc
-	{"HsN4DLc5n7kKyfUMKm5SW56J8LJGTK6u91h", 10000 * 1e8}, //fanlq
-	{"HsF4tYLz9JpUFk9aPLC7U2AN8Deq6LkyoWc", 10000 * 1e8}, //shanyl
-	{"HsZKbCUvcpjfHAJpDfWikD7E2oXUGR4ge6q", 10000 * 1e8}, //panc
-	{"HsNu7JN9SeNb3cH7BJWMiSSpPqY6rz8BSXW", 10000 * 1e8}, //guxy
-	{"HsamDEnZXPRczM4tNTrKbvUZA8fUSe2TqPk", 10000 * 1e8}, //dengcg
-	{"HsLwT4E2ZdqMDwtrtaQKqp98wVaHrJfyEYM", 10000 * 1e8}, //lixm
-	{"HsS6Hqt7yB5Fr2HDBz5gRhK75q7ciuxa7au", 10000 * 1e8}, //yaoyq
 }
